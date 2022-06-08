47ebd5591e31da71d7892f058cbf83512b0aa9a3
Luis <foreverluiscastro@gmail.com>
2022-06-08 13:38:53 -0400

    Completed core and stretch deliverables.

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState , useEffect } from "react";
 
 import Filters from "./Filters";
 import PetBrowser from "./PetBrowser";
@@ -6,6 +6,39 @@ import PetBrowser from "./PetBrowser";
 function App() {
   const [pets, setPets] = useState([]);
   const [filters, setFilters] = useState({ type: "all" });
+  const [dropdownSelect, setDropdownSelect] = useState("")
+
+  const API = 'http://localhost:3001/pets'
+
+  useEffect(() => {
+    if (filters.type === "all") {
+      fetch(API)
+      .then(r => r.json())
+      .then(pets => setPets(pets))
+    } else {
+      fetch(`http://localhost:3001/pets?type=${filters.type}`)
+      .then(r => r.json())
+      .then(pets => setPets(pets))
+    }
+  },[filters.type]);
+
+  function handleCategoryChange(e) {
+    setDropdownSelect(e.target.value)
+  }
+  
+  function handleFindPets() {
+    setFilters({type: dropdownSelect })
+  }
+
+  function handleAdopt(petId) {
+    console.log(petId)
+    const foundPet = pets.find(pet => pet.id === petId)
+    const index = pets.indexOf(foundPet)
+    foundPet.isAdopted = true
+    const updatedList = pets
+    updatedList[index] = foundPet
+    setPets(updatedList)
+  }
 
   return (
     <div className="ui container">
@@ -15,10 +48,10 @@ function App() {
       <div className="ui container">
         <div className="ui grid">
           <div className="four wide column">
-            <Filters />
+            <Filters onChangeType={handleCategoryChange} onFindPetsClick={handleFindPets}/>
           </div>
           <div className="twelve wide column">
-            <PetBrowser />
+            <PetBrowser pets={pets} onAdoptPet={handleAdopt}/>
           </div>
         </div>
       </div>
